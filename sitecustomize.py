8d7f587f5459c1ee1e32b152d7bb5f01bacb73a7
cheng07021028 <cheng07021028@gmail.com>
2026-06-03 16:58:06 +0800

    Delete sitecustomize.py

--- sitecustomize.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""V160 startup hook: set app timezone before Streamlit loads pages."""
-try:
-    from services.timezone_bootstrap_service import apply_app_timezone
-    apply_app_timezone()
-except Exception:
-    pass
